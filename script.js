eef4203199e72059d02f17516f1572a62ca4d32b
kawaldhillon05 <kawaldhillon5@gmail.com>
2024-02-18 01:28:36 -0500

    all methods functional

--- script.js
+++ script.js
@@ -13,55 +13,121 @@ class LinkedList {
   constructor(){
 
     this.head = null;
-
-  }
-
-  lastNode(node){
-    if(node.next === null){
-      return node;
-    } 
-  
-    return this.lastNode(node.next);
+    this.last = null;
+    this.length = 0;
   
   }
-
 
   append(value){
 
-    if(this.head === null){
-      this.head = new Node(value);
-      return;
-    } 
-
-    let nullNode = this.lastNode(this.head);
-    nullNode.next = new Node(value);
+    if (this.last) {
+      this.last = this.last.next = new Node(value);
+    } else {
+      this.head = this.last = new Node(value);
+    }
+    this.length++;
 
   }
 
   prepend(value){
 
-    const node = new Node(value, this.head);
-    this.head = node;
+    if (this.head) {
+      this.head  = new Node(value, this.head);
+    } else {
+      this.head = this.last = new Node(value);
+    }
+    this.length++;
 
   }
 
   size(){
-
-    let length = 0;
-    let node = this.head;
-    while(!(node === null)){
-      node = node.next;
-      ++length;
-    }
-    return length;
+    return this.length;
   }
 
   first (){
-
     return this.head;
-
   }
 
+  tail(){
+    return this.last;
+  }
+
+  at(index){
+
+    let node = this.head;
+    for(let i = 0; i < index; i++){
+      node = node.next
+    }
+    return node;
+  }
+
+  pop(){
+
+    const value = this.last.value;
+    if (this.head == null){
+      return;
+    }
+    else if (this.head == this.last){
+      this.head = null;
+      this.last = null;
+      this.length--
+    }
+    else {
+      let temp = this.head;
+      let pre = this.head;
+      while(temp.next){
+          pre = temp;
+          temp = temp.next;
+      }
+      this.last = pre;
+      this.last.next = null;
+      this.length--
+    }
+    return value;
+  }
+
+  contains(input) {
+
+    let node = this.head;
+    let found = false;
+    for(let i = 0; i <= this.length; i++){
+      if(node){
+        if(node.value === input){
+          found = true;
+        }
+        node = node.next;
+      }
+    }
+    return found;
+  }
+
+  find(input){
+    let node = this.head;
+    let index = null;
+    for(let i = 1; i <= this.length; i++){
+      if(node){
+        if(node.value === input){
+          index =  i
+        } 
+        node = node.next;
+      }
+    }
+    return index;
+  }
+
+  toString(){
+    let node = this.head;
+    let value = "";
+    for(let i = 0; i <= this.length; i++){
+      if(node === null){
+          value += `null`; 
+      } else {
+        value += `(${node.value}) -> `;
+        node = node.next;
+      }
+    }
+    return value;
+  }
 
 }
 
@@ -77,7 +143,10 @@ list.prepend("one");
 list.append("four");
 list.append("five")
 
-console.log(list.size())
-console.log(list.first());
-console.log(list.lastNode(list.head))
+// console.log(list.size())
+// console.log(list.first());
+// console.log(list.tail())
+// console.log(list.at(2));
+// list.pop();
+console.log(list.toString());
 console.log(list);
